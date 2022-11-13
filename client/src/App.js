dcd8138309420ad77c9b7c2f27f0e5fa4de15436
Verah Nyagaka <verahnyagaka101@gmail.com>
2022-11-13 23:17:25 +0300

    added home

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,15 @@ import {
 } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return(
     <div>
     <Router>
   <Navbar />
-  <Routes>
-    <Route path="/" element={<Home />} />
-  </Routes>
+  <Home/>
+  <About/>
 </Router>
   </div>
   )
