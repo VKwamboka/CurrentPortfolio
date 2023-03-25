c01020f520b3293406fb82f66a618d3784467023
Verah Kwamboka <verahnyagaka101@gmail.com>
2023-03-25 23:49:09 +0300

    updated my portfolio

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -9,6 +9,10 @@ import FireBase from '../assets/firebase.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
 import Mongo from '../assets/mongo.png';
+import Rails from '../assets/rails.png';
+import Angular from '../assets/angular.png';
+import Sinatra from '../assets/sinatra.png';
+
 
 const Skills = () => {
   return (
@@ -59,15 +63,15 @@ const Skills = () => {
                   <p className='my-4'>FIREBASE</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={FireBase} alt="HTML icon" />
+                  <img className='w-20 mx-auto' src={Angular} alt="HTML icon" />
                   <p className='my-4'>ANGULAR</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={FireBase} alt="HTML icon" />
+                  <img className='w-20 mx-auto' src={Rails} alt="HTML icon" />
                   <p className='my-4'>RAILS</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={FireBase} alt="HTML icon" />
+                  <img className='w-20 mx-auto' src={Sinatra} alt="HTML icon" />
                   <p className='my-4'>SINATRA</p>
               </div>
           </div>
